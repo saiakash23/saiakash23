87840d8c560a58c0c0df45659bf8fe2f5be3b2dd
saiakash23 <94002413+saiakash23@users.noreply.github.com>
2022-12-22 22:52:16 +0530

    Update python project.py

--- python project.py
+++ python project.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
 
 import random
 num1 =random.randrange(1000, 10000)
@@ -48,16 +43,6 @@ else:
     if n2 == num1:
         print("You've become a Mastermind!")
         print("It took you only", ctr, "tries.")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
